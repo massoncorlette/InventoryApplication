7e18a1f76d7b616d14526da6aec0cab09a98ed72
Masson Corlette <massoncorlette07@gmail.com>
2025-03-24 18:37:31 -0400

    chore:adding my env variables and setting up Pool for node.js PostgreSQL connection

---
+++ db/pool.js
@@ -0,0 +1,11 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+// All of the following properties should be read from environment variables
+module.exports = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
+});
