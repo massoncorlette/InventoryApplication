351d57cbd8c147ab95ed2d50513762c2f4120883
massoncorlette <massoncorlette07@gmail.com>
2025-05-15 02:45:57 -0400

    refactor: updating pool logic to attempt to connect to postgre service on railway

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+const connectionString = "postgresql://postgres:dXSUNYYNXEmCIPCJcmZUhzChkniqzLHm@crossover.proxy.rlwy.net:15397/railway";
+
 // All of the following properties should be read from environment variables
 module.exports = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString,
 });
