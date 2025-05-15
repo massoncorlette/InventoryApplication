a56c7f5f2000d5e3ebd25acdfe3338f0e45cb5fd
massoncorlette <massoncorlette07@gmail.com>
2025-05-15 04:19:47 -0400

    final push?

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const connectionString = "postgresql://postgres:dXSUNYYNXEmCIPCJcmZUhzChkniqzLHm@crossover.proxy.rlwy.net:15397/railway";
 
-// All of the following properties should be read from environment variables
 module.exports = new Pool({
-  connectionString,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
+
+
